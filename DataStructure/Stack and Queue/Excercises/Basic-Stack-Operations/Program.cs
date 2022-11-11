96dc7d5311be372602bce59b17586a8beefb1116
pavelnanev <82061050+obscureTime@users.noreply.github.com>
2022-11-11 11:04:07 -0800

    mod

---
+++ DataStructure/Stack and Queue/Excercises/Basic-Stack-Operations/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class BasicStackOperations
+{
+
+    static void Main()
+    {
+        string[] args = Console.ReadLine().Split();
+
+        int countElementsToPush = int.Parse(args[0]);
+        int countElementsToPop = int.Parse(args[1]);
+        int elementToFind = int.Parse(args[2]);
+
+        int[] collection = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+
+        Stack<int> numbers = new Stack<int>();
+
+        PushMany(countElementsToPush, collection, numbers);
+        PopMany(countElementsToPop, numbers);
+
+        if (numbers.Count == 0)
+        {
+            Console.WriteLine("0");
+            return;
+        }
+        else
+        {
+            if (numbers.Contains(elementToFind))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(numbers.Min());
+            }
+        }
+    }
+
+    private static void PopMany(int countElementsToPop, Stack<int> numbers)
+    {
+        for (int i = 0; i < countElementsToPop; i++)
+        {
+                numbers.Pop();
+        }
+       
+    }
+
+    private static void PushMany(int countElementsToPush, int[] collection, Stack<int> numbers)
+    {
+        for (int index = 0; index < countElementsToPush; index++)
+        {
+            numbers.Push(collection[index]);
+
+        }
+    }
+
+    
+}
+
+
