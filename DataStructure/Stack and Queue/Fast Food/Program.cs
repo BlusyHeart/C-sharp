4b90cf4f11cd261f5e5d0c9cdadced0cedbe3d37
pavelnanev <82061050+obscureTime@users.noreply.github.com>
2022-11-11 13:17:36 -0800

    FoodService

---
+++ DataStructure/Stack and Queue/Fast Food/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[]args)
+    {
+
+        int totalFoodQuantity = int.Parse(Console.ReadLine());
+
+        List<int> sequanceOfOrders = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToList();
+
+        Queue<int> orders = new Queue<int>(sequanceOfOrders);
+        Console.WriteLine(orders.Max());
+        int countOrders = orders.Count;
+
+        for (int i = 1; i <= countOrders; i++)
+        {
+            if (totalFoodQuantity >= orders.Peek())
+            {
+                totalFoodQuantity -= orders.Peek();
+                orders.Dequeue();
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        if (orders.Count == 0)
+        {
+            Console.WriteLine("Orders complete");            
+        }
+        else
+        {
+            Console.WriteLine($"Orders left: {string.Join("", orders)}");
+        }
+
+    }
+}
