f2f3c94782f9de40322a3e9988421d2dbdb698ec
pavelnanev <82061050+obscureTime@users.noreply.github.com>
2022-11-12 13:27:51 -0800

    add excercise

---
+++ DataStructure/Excercises/Multidimensional Arrays/Sum Matrix Elements/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int [,]matrix = CreateMatrix();  
+        matrix = FillMatrix(matrix);
+
+        Console.WriteLine(Sum(matrix));
+
+    }
+
+    private static int[,] CreateMatrix()
+    {
+        int[] matrixDimensions = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+        int rows = matrixDimensions[0];
+        int columns = matrixDimensions[1];
+        int[,] matrix = new int[rows, columns];
+
+        return matrix;
+    }
+
+    private static int Sum(int[,] matrix)
+    {
+        int sum = 0;
+        foreach (int number in matrix)
+        {
+            sum += number;
+        }
+
+        return sum;
+    }
+
+
+    private static int[,] FillMatrix(int[,] matrix)
+    {       
+        for (int rows = 0; rows < matrix.GetLength(0); rows++)
+        {
+            int[] input = Console.ReadLine()
+            .Split(", ")
+            .Select(int.Parse)
+            .ToArray();
+
+            for (int columns = 0; columns < matrix.GetLength(1); columns++)
+            {
+                matrix[rows, columns] = input[columns];
+            }
+        }
+
+        return matrix;
+    }
+}
