fb411a1664b278c2dacbe0ac34081bcb60ac547b
pavelnanev <82061050+obscureTime@users.noreply.github.com>
2022-11-13 12:30:59 -0800

    added

---
+++ DataStructure/Excercises/Dictionaries/CountSameValuesinArray/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<double, int> collectionOfNumbers = new Dictionary<double, int>();
+
+        double[] numbers = Console.ReadLine()
+            .Split()
+            .Select(double.Parse)
+            .ToArray();
+
+        for (int index = 0; index < numbers.Length; index++)
+        {
+            double currentNumber = numbers[index];
+
+            if (!collectionOfNumbers.ContainsKey(currentNumber))
+            {
+                collectionOfNumbers.Add(currentNumber, 0);
+            }
+            collectionOfNumbers[currentNumber]++;
+
+        }
+        PrintKeyValue(collectionOfNumbers);
+
+    }
+
+    private static void PrintKeyValue(Dictionary<double, int> collectionOfNumbers)
+    {
+        foreach (KeyValuePair<double, int> key in collectionOfNumbers)
+        {
+            Console.WriteLine($"{key.Key} - {key.Value} times");
+        }
+    }
+}
+
---
+++ DataStructure/Excercises/Multidimensional Arrays/SnakeMoves/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+
+        int[] dimensions = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+
+        int rows = dimensions[0];
+        int cols = dimensions[1];
+
+        char[,] matrix = new char[rows, cols];
+
+        string snake = Console.ReadLine();
+
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                    matrix[row, col] = snake[col];
+            }
+        }
+    }
+}
