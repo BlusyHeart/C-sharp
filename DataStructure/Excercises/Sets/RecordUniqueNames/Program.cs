17842759c7c581ae24590159839c244f1d6966ff
pavelnanev <82061050+obscureTime@users.noreply.github.com>
2022-11-13 15:06:33 -0800

    added

---
+++ DataStructure/Excercises/Sets/RecordUniqueNames/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        HashSet<string> names = new HashSet<string>();       
+        FillHashSet(names);
+        Console.WriteLine(String.Join(Environment.NewLine, names));
+    }
+
+    private static void FillHashSet(HashSet<string> set)
+    {
+        int numberOfInputs = int.Parse(Console.ReadLine());
+        for (int i = 0; i < numberOfInputs; i++)
+        {
+            set.Add(Console.ReadLine());
+        }
+    }
+}
