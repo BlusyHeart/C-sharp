6aa8016ce00c332afc09f3dabdd2b65c8ba73b81
pavelnanev <82061050+obscureTime@users.noreply.github.com>
2022-11-14 06:58:21 -0800

    added

---
+++ DataStructure/Excercises/Sets/ParkingLot/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        HashSet<string> carNumbers = new HashSet<string>();
+        CardNumberController(carNumbers);
+    }
+    private static void CardNumberController(HashSet<string> carNumbers)
+    {
+        while (true)
+        {
+            string[] input = ReadInput();
+            string direction = input[0];
+            if (direction == "END")
+            {
+                break;
+            }
+            string carNumber = input[1];
+            ChooseDirection(carNumbers, direction, carNumber);
+        }
+        if (ValidateCount(carNumbers))
+        {
+            PrintCardNumbers(carNumbers);
+        }
+        else
+        {
+            Console.WriteLine("Parking Lot is Empty");
+        }
+    }
+
+    private static bool ValidateCount(HashSet<string> carNumbers) => carNumbers.Any();
+    private static void PrintCardNumbers(HashSet<string> carNumbers)
+    {
+        foreach (string carNumber in carNumbers)
+        {
+            Console.WriteLine(carNumber);
+        }
+    }
+
+    private static void ChooseDirection(HashSet<string> carNumbers, string direction, string carNumber)
+    {
+        switch (direction)
+        {
+            case "IN":
+                CheckIN(carNumbers, carNumber);
+                break;
+            case "OUT":
+                CheckOUT(carNumbers, carNumber);
+                break;
+
+        }
+    }
+
+    private static void CheckOUT(HashSet<string> carNumbers, string carNumber) => carNumbers.Remove(carNumber);
+    private static void CheckIN(HashSet<string> carNumbers, string carNumber) => carNumbers.Add(carNumber);    
+    private static string[] ReadInput() => Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+}
--- DataStructure/Excercises/Sets/RecordUniqueNames/Program.cs
+++ DataStructure/Excercises/Sets/RecordUniqueNames/Program.cs
@@ -6,17 +6,13 @@ class Program
 {
     static void Main()
     {
-        HashSet<string> names = new HashSet<string>();       
-        FillHashSet(names);
-        Console.WriteLine(String.Join(Environment.NewLine, names));
+
+
+
+
+
+
     }
 
-    private static void FillHashSet(HashSet<string> set)
-    {
-        int numberOfInputs = int.Parse(Console.ReadLine());
-        for (int i = 0; i < numberOfInputs; i++)
-        {
-            set.Add(Console.ReadLine());
-        }
-    }
+
 }
