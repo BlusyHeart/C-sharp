61ab777d4f263014d0426c761e97d538d67b8972
pavelnanev <82061050+obscureTime@users.noreply.github.com>
2022-11-15 13:11:11 -0800

    added

---
+++ Functional Programming/CustomMinFunction/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {       
+        int[] numbers = Console.ReadLine()
+            .Split(" ")
+            .Select(int.Parse)
+            .ToArray();
+
+        Func<int[], int> minNumber = number =>
+        {
+            int minValue = int.MaxValue;
+            foreach (int item in numbers)
+            {
+                if (item < minValue)
+                {
+                    minValue = item;
+                }
+            }
+            return minValue;
+        };
+
+        Console.WriteLine(minNumber(numbers));        
+    }
+}
