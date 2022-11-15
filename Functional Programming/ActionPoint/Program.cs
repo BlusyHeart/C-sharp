ef27713e49fe0470de7ccfd66d43050e24b3ab2f
pavelnanev <82061050+obscureTime@users.noreply.github.com>
2022-11-15 12:24:14 -0800

    added

---
+++ Functional Programming/ActionPoint/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string[] names = Console.ReadLine().Split();
+
+        Action<string> printer = name => Console.WriteLine(name);
+        Print(names, printer);
+
+    }
+
+    private static void Print(string[] names, Action<string> printer)
+    {
+        foreach (string name in names)
+        {
+            printer(name);
+        }
+    }
+}
