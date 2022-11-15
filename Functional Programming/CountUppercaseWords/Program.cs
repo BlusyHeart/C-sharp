5fb1402b56d43d6d0db7eacb3827c1567929c441
pavelnanev <82061050+obscureTime@users.noreply.github.com>
2022-11-15 08:32:34 -0800

    added

---
+++ Functional Programming/CountUppercaseWords/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{   
+    static void Main()
+    {
+        Func<string, bool> startsWithUpperCase = word => char.IsUpper(word[0]);
+       
+        List<string> text = Console.ReadLine()
+            .Split(new[] {',',',','.',' '}, StringSplitOptions.RemoveEmptyEntries)
+            .Where(startsWithUpperCase)
+            .ToList();
+
+        Console.WriteLine(String.Join(Environment.NewLine, text));
+    }
+}
