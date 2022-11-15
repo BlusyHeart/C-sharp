04994adf6f67746aeda0e79c37beae439cb2439d
pavelnanev <82061050+obscureTime@users.noreply.github.com>
2022-11-15 07:12:24 -0800

    added

---
+++ Functional Programming/SortEvenNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = Console.ReadLine()
+            .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .Where(x => x % 2 == 0)
+            .OrderBy(x => x)
+            .ToList();
+
+        Console.WriteLine(String.Join(", ", numbers));
+            
+
+
+        
+
+
+       
+    }
+    
+}
