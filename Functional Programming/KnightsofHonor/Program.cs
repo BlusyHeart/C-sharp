04a18d7f84a583d9b009fcfcd3d0e5ddb25c32f9
pavelnanev <82061050+obscureTime@users.noreply.github.com>
2022-11-15 12:48:38 -0800

    added

---
+++ Functional Programming/KnightsofHonor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Action<string> printer = name => Console.WriteLine($"Sir {name}");
+
+        string[] names = Console.ReadLine().Split();
+        Print(names, printer);
+    }
+
+    static void Print(string[] names, Action<string> printer)
+    {
+        foreach (string name in names)
+        {
+            printer(name);
+        }
+    }
+   
+}
