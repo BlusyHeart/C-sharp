72c735062cea917acf4f32e20a07912ac1079f6b
pavelnanev <82061050+obscureTime@users.noreply.github.com>
2022-11-16 08:22:11 -0800

    added

---
+++ Functional Programming/ReverseandExclude/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Func<int, int, bool> isDivisible = (x, y) => x % y == 0;
+        Action<int[]> printer = numbers => Console.WriteLine(String.Join(" ", numbers));
+
+        List<int> numbers = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .Reverse()
+            .ToList();
+
+        int targetNumber = int.Parse(Console.ReadLine());
+
+        int[] filteredNumbers = numbers.Where(x => !isDivisible(x, targetNumber)).ToArray();
+        printer(filteredNumbers);
+    }
+}
