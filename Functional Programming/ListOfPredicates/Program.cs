b6b6e497632667a63ecf3d5b283c4e17adc4db30
pavelnanev <82061050+obscureTime@users.noreply.github.com>
2022-11-16 09:23:47 -0800

    added

---
+++ Functional Programming/ListOfPredicates/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+
+        Action<int[]> printer = numbers => Console.WriteLine(String.Join(" ", numbers));
+        Func<int, int[], bool> isDivisbile = (number, dividers) =>
+        {          
+            foreach (int divider in dividers)
+            {
+                bool isDivisbile = false;
+                if (number % divider == 0)
+                {
+                    isDivisbile = true;                   
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        };
+
+        int endRange = int.Parse(Console.ReadLine());
+        int[] rangeOfNumbers = Enumerable.Range(1, endRange).ToArray();
+
+        int[] dividers = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+
+        int[] newArray = rangeOfNumbers.Where(x => isDivisbile(x, dividers)).ToArray();
+        printer(newArray);
+    }
+}
---
+++ Functional Programming/PredicateforNames/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Func<string, int, bool> isLessOrEqual = (name, targetLength) => name.Length <= targetLength;
+        Action<string[]> printer = numbers => Console.WriteLine(String.Join(Environment.NewLine, numbers));
+
+        int targetLength = int.Parse(Console.ReadLine());
+        string []names = Console.ReadLine().Split();
+
+        string[] filteredNames = names.Where(x => isLessOrEqual(x, targetLength)).ToArray();
+
+        printer(filteredNames);
+    }
+}
