d5a9e171cc1e357c291a0ef954f6351e6bb9f3b1
pavelnanev <82061050+obscureTime@users.noreply.github.com>
2022-11-17 04:56:31 -0800

    added

---
+++ OOP/Excercises/SpeedRacing/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedRacing
+{
+    public class Car
+    {       
+        public string Model { get; set; }
+
+        public double FuelAmount { get; set; }
+
+        public double FuelConsumptionPerKilometer { get; set; }
+
+        public double TravelledDistance { get; set; } = 0;
+
+        public void Drive(double distance)
+        {
+            bool isEnough = FuelAmount >= FuelConsumptionPerKilometer * distance;
+            if (isEnough)
+            {
+                TravelledDistance += distance;
+                FuelAmount -= distance * FuelConsumptionPerKilometer;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+    }
+}
---
+++ OOP/Excercises/SpeedRacing/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SpeedRacing
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int numberOfCars = int.Parse(Console.ReadLine());
+            HashSet<Car> cars = new HashSet<Car>();
+
+            AddCar(numberOfCars, cars);
+            Move(cars);
+            Print(cars);
+        }
+
+        private static void Print(HashSet<Car> cars)
+        {
+            foreach (Car car in cars)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TravelledDistance}");
+            }
+        }
+
+        private static void Move(HashSet<Car> cars)
+        {
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split();
+                if (input[0] == "End")
+                {
+                    break;
+                }
+
+                if (input[0] == "Drive")
+                {
+                    string model = input[1];
+                    double distance = double.Parse(input[2]);
+                    
+                    foreach (Car car in cars)
+                    {
+                        if (car.Model == model)
+                        {
+                            car.Drive(distance);
+                            break;
+                        }
+                    }
+
+                }
+            }
+        }
+
+        private static void AddCar(int numberOfCars, HashSet<Car> cars)
+        {
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                Car car = new Car();
+                string[] carDetails = Console.ReadLine().Split();
+                string carModel = carDetails[0];
+                double fuelAmount = double.Parse(carDetails[1]);
+                double fuelConsumption = double.Parse(carDetails[2]);
+
+                car.Model = carModel;
+                car.FuelAmount = fuelAmount;
+                car.FuelConsumptionPerKilometer = fuelConsumption;
+                cars.Add(car);
+            }
+        }
+    }
+}
+
+
+
