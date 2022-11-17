44b6103c40c39ccd29fd2b791d2d862ee9a3df8e
pavelnanev <82061050+obscureTime@users.noreply.github.com>
2022-11-17 03:43:42 -0800

    added

---
+++ OOP/Excercises/DateModifier/DateModifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateModifier
+{
+    public static class DateModifier
+    {
+
+        public static int DifferenceInDays(string firsData, string secondData)
+        {
+
+            DateTime fistDate = DateTime.Parse(firsData);
+            DateTime secondDate = DateTime.Parse(secondData);
+
+            int days = Math.Abs((fistDate - secondDate).Days);
+            return days;
+        }
+
+    }
+}
---
+++ OOP/Excercises/DateModifier/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DateModifier
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            int days = DateModifier.DifferenceInDays(firstDate, secondDate);
+
+            Console.WriteLine(days);
+
+        }
+    }
+}
