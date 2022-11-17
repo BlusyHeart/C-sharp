427ce12606f6df6666dd7831f6e2096c2f33b1f2
pavelnanev <82061050+obscureTime@users.noreply.github.com>
2022-11-17 03:05:50 -0800

    added

---
+++ OOP/Excercises/DefinaAClassPerson/Family.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public Family()
+        {
+            FamilyMembers = new List<Person>();
+        }
+
+        public List<Person> FamilyMembers { get; set; }
+        
+        public void AddMember(Person member)
+        {
+            FamilyMembers.Add(member);
+        }
+
+        public Person GetOldestMember() => FamilyMembers.OrderByDescending(person => person.Age).FirstOrDefault();
+
+        public List<Person> GetOlderThen30Years()
+        {
+            List<Person> filterFamalyMembers = FamilyMembers.Where(member => member.Age > 30).ToList();
+            return filterFamalyMembers;
+        }
+        public void PrintMember(List<Person> persons)
+        {
+            foreach (Person person in persons)
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
---
+++ OOP/Excercises/DefinaAClassPerson/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int numberOfMembers = int.Parse(Console.ReadLine());
+            Family family = new Family();
+
+            for (int index = 0; index < numberOfMembers; index++)
+            {
+                string[] memberInfo = Console.ReadLine()
+                    .Split()
+                    .ToArray();
+
+                string name = memberInfo[0];
+                int age = int.Parse(memberInfo[1]);
+
+                Person person = new Person(name, age);
+                family.AddMember(person);
+               
+            }
+
+            List<Person> filteredMembers = family.GetOlderThen30Years();
+            List<Person> sortedMembers = filteredMembers.OrderBy(member => member.Name).ToList();
+            family.PrintMember(sortedMembers);
+
+        }
+
+    }
+}
+
+
+
