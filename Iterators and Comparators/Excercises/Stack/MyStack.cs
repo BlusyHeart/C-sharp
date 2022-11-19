e7e5094f6090187a73e0ad71b8684bfee92bc08e
pavelnanev <82061050+obscureTime@users.noreply.github.com>
2022-11-19 08:18:00 -0800

    added

---
+++ Iterators and Comparators/Excercises/Stack/MyStack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class MyStack<T> : IEnumerable<T> 
+    {
+        private List<T> collection;
+
+        public MyStack()
+        {
+            this.collection = new List<T>();
+        }
+
+        public void Push(T item) => collection.Add(item);
+       
+        public T Pop()
+        {
+            if (collection.Count == 0)
+            {
+                throw new ArgumentException("No elements");
+            }
+
+            T element = collection.Last();
+            collection.RemoveAt(collection.Count - 1);
+            return element;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int index = collection.Count - 1; index >= 0; index--)
+            {
+                yield return collection[index];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Iterators and Comparators/Excercises/Stack/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IteratorsAndComparators
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            MyStack<int> stack = new MyStack<int>();
+            while (true)
+            {
+                char[] delimeters = new char[] { ' ', ',' };
+
+                string[] input = Console.ReadLine().Split(delimeters, StringSplitOptions.RemoveEmptyEntries);
+                string command = input[0];
+
+                if (command == "END")
+                {
+                    break;
+                }
+                else if (command == "Pop")
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (ArgumentException ae)
+                    {
+                        Console.WriteLine(ae.Message);
+                    }
+                }
+                else if (command == "Push")
+                {
+                    int[] elements = input.Skip(1)
+                        .Select(int.Parse)
+                        .ToArray();
+
+                    foreach (int element in elements)
+                    {
+                        stack.Push(element);
+                    }
+                }
+            }
+
+            foreach (int element in stack)
+            {
+                Console.WriteLine(element);
+            }
+            foreach (int element in stack)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
