02e9c30da21d0663a9f7ae67f81274714a5af6b0
pavelnanev <82061050+obscureTime@users.noreply.github.com>
2022-11-25 08:58:07 -0800

    added

---
+++ OOP/Excercises/StreetRacing/Models/Race.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreeRacing.Models
+{
+    internal class Race
+    {
+        public Race(string name, string type, int laps, int capacity, int maxHorsePower)
+        {
+            Name = name;
+            Type = type;
+            Laps = laps;
+            Capacity = capacity;
+            MaxHorsePower = maxHorsePower;
+            Participants = new List<Car>();
+        }
+
+
+        public List<Car> Participants { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public int Laps { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int MaxHorsePower { get; set; }
+
+        public void Count() => Participants.Count();
+
+        public void AddCar(Car car)
+        {
+            if (CheckLicensePlate(car.LicensePlate) && CheckCapacity() && CheckHorsePower(car.HorsePower))
+            {
+                Participants.Add(car);
+            }
+        }
+
+        private bool CheckLicensePlate(string licensePlate) =>
+            !Participants.Any(x => x.LicensePlate == licensePlate);
+
+        private bool CheckCapacity() => Participants.Count + 1 <= Capacity;
+
+        private bool CheckHorsePower(double horsePower) => horsePower <= MaxHorsePower;
+
+        public bool RemoveCar(string licensePlate)
+            => Participants.Remove(Participants.Find(x => x.LicensePlate == licensePlate));
+
+        public Car FindCar(string licensePlate) => Participants.FirstOrDefault(x => x.LicensePlate == licensePlate);
+
+        public Car GetMostPowerfulCar()
+            => Participants.OrderByDescending(x => x.HorsePower).FirstOrDefault();
+
+        public void Report()
+        {
+            Console.WriteLine($"Race: {Name} - Type: {Type} (Laps: {Laps})\n{string.Join(Environment.NewLine, Participants)}");
+        }
+    }
+}
---
+++ OOP/Excercises/StreetRacing/Program.cs
@@ -0,0 +1,33 @@
+﻿using StreeRacing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StreeRacing
+{
+    class StartUp
+    {
+        static void Main()
+        {
+
+            Race race = new Race("RockPort Race", "Sprint", 1, 4, 150);
+
+            Car car = new Car("BMW", "320ci", "NFS2005", 150, 1450);
+
+            Console.WriteLine(car);
+
+            race.AddCar(car);
+            Console.WriteLine();
+            Console.WriteLine(race.RemoveCar("NFS2005"));
+
+            Car carOne = new Car("BMW", "320cd", "NFS2007", 150, 1350);
+            Car carTwo = new Car("Audi", "A3", "NFS2004", 131, 1300);
+
+            race.AddCar(carOne);
+            race.AddCar(carTwo);
+           
+            Console.WriteLine(race.GetMostPowerfulCar());
+            race.Report();
+        }
+    }
+}
