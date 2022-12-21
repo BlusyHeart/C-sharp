9cd35b2f8cc207a9ec94303a65d3b8a0c516d6a3
pavelnanev <82061050+obscureTime@users.noreply.github.com>
2022-12-21 18:57:18 +0200

    added

---
+++ RegularExpressions/MatchPhoneNumber/MatchDates.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Regex_Training
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            string pattern = @"\+359([ |-])2\1\d{3}\1\d{4}\b";
+
+            string phones = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(phones);
+
+            Console.WriteLine(string.Join(", ", matches));
+        }
+    }
+}
