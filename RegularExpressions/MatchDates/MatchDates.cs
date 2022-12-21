a70b0bdc782d5776dd026f15de7766e8a3103f1f
pavelnanev <82061050+obscureTime@users.noreply.github.com>
2022-12-21 18:55:13 +0200

    added

---
+++ RegularExpressions/MatchDates/MatchDates.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Regex_Training
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            string pattern = @"(?<day>\d{2})([.|/|-])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})";
+
+            string dates = Console.ReadLine();   
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(dates);
+
+            foreach (Match match in matches)
+            {
+                string day = match.Groups["day"].Value;
+                string month = match.Groups["month"].Value;
+                string year = match.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }  
+}
