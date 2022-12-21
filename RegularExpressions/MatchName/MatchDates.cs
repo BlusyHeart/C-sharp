1b1a33b07a7d5f7685fd39629b4519bc273731dc
pavelnanev <82061050+obscureTime@users.noreply.github.com>
2022-12-21 18:56:39 +0200

    added

---
+++ RegularExpressions/MatchName/MatchDates.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Regex_Training
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            string names = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(names);
+
+            foreach (Match match in matches)
+            {
+                Console.Write($"{match.Value} ");
+            }
+        }
+    }
+}
